95f543d7c9bc57ac771762b62e2af0eca6f6f9ef
diane <villalino.136534100519@depedqc.ph>
2024-03-23 21:05:58 +0800

    Pseudocode

---
+++ Information_Fancy.py
@@ -0,0 +1,6 @@
+# Importing necessary modules
+
+# Asking for the user input
+# Generating fancy ASCII art for user's information
+# Applying color to the generated ASCII art text
+# Printing user's information together with ASCII art and color
